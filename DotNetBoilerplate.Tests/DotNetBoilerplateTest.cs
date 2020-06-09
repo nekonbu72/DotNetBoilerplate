71af8011f25a57048c7900541bfd370327277fcc
nekonbu72 <nakatomo222@gmail.com>
2020-06-09 22:29:48 +0900

    .

---
+++ DotNetBoilerplate.Tests/DotNetBoilerplateTest.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace DotNetBoilerplate.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            Hello.Main();
+        }
+    }
+}
---
+++ DotNetBoilerplate/DotNetBoilerplate.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace DotNetBoilerplate
+{
+    public class Hello
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Hello, DotNet!");
+
+            var startInfo = new ProcessStartInfo
+            {
+                FileName = @"C:\Program Files\dotnet\dotnet.exe",
+                CreateNoWindow = true,
+                UseShellExecute = false,
+                RedirectStandardOutput = true,
+                RedirectStandardInput = false
+            };
+
+            using (var process = Process.Start(startInfo))
+            {
+                var output = process.StandardOutput.ReadToEnd();
+                process.WaitForExit();
+                Console.WriteLine(output);
+            }
+
+
+        }
+    }
+}
